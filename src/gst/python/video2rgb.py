a0fc4d309c3f892cb66b0bf675342602b7568633
Kipp Cannon <kcannon@cita.utoronto.ca>
2013-06-21 01:19:14 -0400

    video2rgb:  don't fake frame rates
    now that we have audiorationalresample this element doesn't have to lie
    about the time series sample rate

--- src/gst/python/video2rgb.py
+++ src/gst/python/video2rgb.py
@@ -9,23 +9,14 @@ pygst.require('0.10')
 import gst
 
 
+from cardiacam.videotypes import video_caps, video_dtype
+
+
 __author__ = "Kipp Cannon <kipp.cannon@ligo.org>"
 __version__ = "FIXME"
 __date__ = "FIXME"
 
 
-video_caps = (
-	"video/x-raw-rgb, " +
-	"bpp = (int) 24, " +
-	"depth = (int) 24, " +
-	"red_mask = (int) 16711680, " +
-	"green_mask = (int) 65280, " +
-	"blue_mask = (int) 255, " +
-	"endianness = (int) 4321"
-)
-
-
-video_dtype = numpy.dtype({"names": ["r", "g", "b"], "formats": [numpy.uint8, numpy.uint8, numpy.uint8]})
 timeseries_dtype = numpy.dtype({"names": ["r", "g", "b"], "formats": [numpy.double, numpy.double, numpy.double]})
 
 
@@ -75,7 +66,7 @@ class video2rgb(gst.BaseTransform):
 				"audio/x-raw-float, " +
 				"channels = (int) 3, " +
 				"endianness = (int) BYTE_ORDER, " +
-				"rate = (int) [1, MAX], " +
+				"rate = (fraction) [0/1, 2147483647/1], " +
 				"width = (int) 64"
 			)
 		)
@@ -102,7 +93,6 @@ class video2rgb(gst.BaseTransform):
 
 
 	def do_set_caps(self, incaps, outcaps):
-		self.framerate = incaps[0]["framerate"]
 		self.width = incaps[0]["width"]
 		self.height = incaps[0]["height"]
 		y, x = numpy.indices((self.height, self.width), dtype = "double")
@@ -161,21 +151,16 @@ class video2rgb(gst.BaseTransform):
 
 
 	def do_transform_caps(self, direction, caps):
-		#
-		# FIXME:  this is hacked to round fractional frame rates to
-		# integer audio sample rates
-		#
-
 		if direction == gst.PAD_SRC:
 			#
 			# convert src pad's caps to sink pad's
 			#
 
-			framerate, = [struct["rate"] for struct in caps]
+			rate, = [struct["rate"] for struct in caps]
 			result = gst.Caps()
 			for struct in self.get_pad("sink").get_pad_template_caps():
 				struct = struct.copy()
-				struct["framerate"] = gst.Fraction(framerate, 1)
+				struct["framerate"] = rate
 				result.append_structure(struct)
 			return result
 
@@ -188,7 +173,7 @@ class video2rgb(gst.BaseTransform):
 			result = gst.Caps()
 			for struct in self.get_pad("src").get_pad_template_caps():
 				struct = struct.copy()
-				struct["rate"] = int(round(float(framerate.num) / float(framerate.denom)))
+				struct["rate"] = framerate
 				result.append_structure(struct)
 			return result
 
