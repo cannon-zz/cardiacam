b1fd64507c1df5caea82cff4a23cb63dcbdb002a
Kipp Cannon <kcannon@cita.utoronto.ca>
2014-01-06 19:22:50 -0500

    fix copyright in cardiacam.c

--- src/gst/cardiacam.c
+++ src/gst/cardiacam.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012  Kipp Cannon, Chad Hanna
+ * Copyright (C) 2013  Kipp Cannon
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
